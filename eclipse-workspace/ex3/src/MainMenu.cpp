9650b6890940df5086de917d57b5770a557909e2
avihayarzuan <33842143+avihayarzuan@users.noreply.github.com>
2017-11-24 10:16:37 +0200

    Add files via upload

---
+++ eclipse-workspace/ex3/src/MainMenu.cpp
@@ -0,0 +1,24 @@
+/*
+ * MainMenu.cpp
+ *
+ *  Created on: Nov 24, 2017
+ *      Author: avihay
+ */
+
+#include "MainMenu.h"
+
+MainMenu::MainMenu() {
+    // TODO Auto-generated constructor stub
+
+}
+
+MainMenu::~MainMenu() {
+    // TODO Auto-generated destructor stub
+}
+
+int MainMenu::run() {
+    int choice;
+
+
+    return choice;
+}
---
+++ eclipse-workspace/ex3/src/MainMenu.h
@@ -0,0 +1,21 @@
+/*
+ * MainMenu.h
+ *
+ *  Created on: Nov 24, 2017
+ *      Author: avihay
+ */
+
+#ifndef MAINMENU_H_
+#define MAINMENU_H_
+
+/*
+ *
+ */
+class MainMenu {
+ public:
+    MainMenu();
+    virtual ~MainMenu();
+    int run();
+};
+
+#endif /* MAINMENU_H_ */
