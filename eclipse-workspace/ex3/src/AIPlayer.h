adf9741c74a51d5da0283ca265506c5b1a8b8e78
avihay arzuan <avihayarzuan@gmail.com>
2017-11-25 19:47:32 +0200

    ai player class

---
+++ eclipse-workspace/ex3/src/AIPlayer.h
@@ -0,0 +1,28 @@
+/*
+ * AIPlayer.h
+ *
+ *  Created on: Nov 25, 2017
+ *      Author: avihay
+ */
+#include "Player.h"
+#include "GameLogic.h"
+#include "MiniMaxLogic.h"
+
+#ifndef AIPLAYER_H_
+#define AIPLAYER_H_
+
+/*
+ *
+ */
+class AIPlayer : public Player {
+ public:
+    AIPlayer(char color);
+    virtual ~AIPlayer() {};
+    char getColor() const;
+    void makeMove(GameLogic *logic, CellMap *posMoves, ConsoleMsgs *printer);
+
+ private:
+    const char color;
+};
+
+#endif /* AIPLAYER_H_ */
